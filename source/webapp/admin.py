c947c34cbd61da4b3ce3c47e549ee7c8e765d1b6
talgar <otalgarbek@gmail.com>
2020-07-30 20:16:50 +0600

    добавлен model товара, создана миграция, и модель зарегистрирован в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter=('category',)
+    list_display=('pk', 'name', 'amount', 'price')
+    list_display=('pk', 'name')
+    search_fields=('name',)
+admin.site.register(Product, ProductAdmin)
